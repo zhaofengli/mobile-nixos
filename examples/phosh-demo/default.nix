7eb2d7a4369722a43800869401e1e8be4db777ce
Zhaofeng Li <hello@zhaofeng.li>
2021-05-02 01:36:51 +0000

    examples/phosh-demo: init

---
+++ examples/phosh-demo/default.nix
@@ -0,0 +1,11 @@
+{ device ? null, pkgs ? null }@args:
+
+import ../../lib/eval-with-configuration.nix (args // {
+  configuration = [ (import ./configuration.nix) ];
+  additionalHelpInstructions = ''
+    You can build the `-A build.default` attribute to build the default output
+    for your device.
+
+     $ nix-build examples/phosh-demo --argstr device ${device} -A build.default
+  '';
+})
