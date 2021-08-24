1f89933e1e60e47be1fb24d5690998c8d483e871
Zhaofeng Li <hello@zhaofeng.li>
2021-08-24 00:23:51 -0700

    system-types/android: Add option to prepend 512-byte MTK header to boot.img

--- modules/system-types/android/bootimg.nix
+++ modules/system-types/android/bootimg.nix
@@ -15,18 +15,36 @@ let
 in
 pkgs.runCommandNoCC name {
   nativeBuildInputs = with buildPackages; [
+    mtk-mkimage
     mkbootimg
     dtbTool
   ];
 } ''
-  echo Using kernel: ${kernel}
+  kernel=${kernel}
+  initrd=${initrd}
+
+  echo ":: Using kernel: $kernel"
+  echo ":: Using initrd: $initrd"
+
+  if [ -n "${builtins.toString bootimg.mtkHeaders}" ]; then
+    echo ":: Prepending MTK headers"
+
+    echo ":: Kernel: $kernel -> kernel-mtk"
+    mtk-mkimage KERNEL $kernel kernel-mtk
+    kernel=kernel-mtk
+
+    echo ":: Kernel: $initrd -> initrd-mtk"
+    mtk-mkimage ROOTFS $initrd initrd-mtk
+    initrd=initrd-mtk
+  fi
+
   (
   PS4=" $ "
   set -x
   mkbootimg \
-    --kernel  ${kernel} \
+    --kernel  $kernel \
     ${optionalString (bootimg.dt != null) "--dt ${bootimg.dt}"} \
-    --ramdisk ${initrd} \
+    --ramdisk $initrd \
     --cmdline       "${cmdline}" \
     --base           ${bootimg.flash.offset_base   } \
     --kernel_offset  ${bootimg.flash.offset_kernel } \
